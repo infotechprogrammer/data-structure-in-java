eba413e320c5940fd408814d562ecb4e26a57f3f
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-03-09 14:16:57 +0530

    Add files via upload

---
+++ _06_Sorting_Problems/_04_Cyclic_Sort_Problems/_05_Find_Missing_Number.java
@@ -0,0 +1,31 @@
+package com.Sorting_Problems._04_Cyclic_Sort_Problems;
+
+public class _05_Find_Missing_Number {
+    public static void main(String[] args) {
+        int[] arr = {2,1,0,6,4,5};
+        System.out.println("Missing Number = "+missingNumber(arr));
+    }
+    static int missingNumber(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i];
+            if(nums[i] < nums.length && nums[i] != nums[correct]) {
+                swap(nums,i,correct);
+            } else { i++; }
+        }
+
+        //search for first missing number
+        for(int index = 0; index < nums.length; index++) {
+            if(nums[index] != index) {
+                return index;
+            }
+        }
+        // case 2
+        return nums.length;
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
