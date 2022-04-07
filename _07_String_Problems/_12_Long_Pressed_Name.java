a1d535e8e96fb0ac7e99e7b53780147f8a38e50b
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-04-07 06:59:25 +0530

    Update _12_Long_Pressed_Name.java

--- _07_String_Problems/_12_Long_Pressed_Name.java
+++ _07_String_Problems/_12_Long_Pressed_Name.java
@@ -1,4 +1,3 @@
-package com.String_Problems;
 /*
 Your friend is typing his name into a keyboard. Sometimes,when typing a character c,
 the key might get long pressed, and the character will be typed 1 or more times.
