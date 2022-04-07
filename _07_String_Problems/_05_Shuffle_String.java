752fd4b4bff391e1ca312ea14b60bcc927ed9cf9
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-04-07 06:59:09 +0530

    Update _05_Shuffle_String.java

--- _07_String_Problems/_05_Shuffle_String.java
+++ _07_String_Problems/_05_Shuffle_String.java
@@ -1,4 +1,3 @@
-package com.String_Problems;
 /*
 You are given a string s and an integer array indices of the same length.
 The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
